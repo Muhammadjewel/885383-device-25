f1995a72aec01a94fd3e7671937e8aed3e401d5b
muhammadjewel <msuratov@gmail.com>
2019-06-26 14:38:50 +0500

    Show only one modal popup at a time

--- js/scripts.js
+++ js/scripts.js
@@ -34,10 +34,18 @@ modalClosingButtons.forEach(function(button) {
   });
 });
 
+var closeAllModals = function() {
+  var modals = document.querySelectorAll('.modal');
+  modals.forEach(function(modal) {
+    modal.classList.remove('shown');
+  });
+};
+
 // Показать карту
 var mapOpener = document.querySelector('.map-link');
 mapOpener.addEventListener('click', function(e) {
   e.preventDefault();
+  closeAllModals();
   var mapModal = document.querySelector('.modal--map');
   mapModal.classList.add('shown');
 });
@@ -46,6 +54,7 @@ mapOpener.addEventListener('click', function(e) {
 var formOpener = document.querySelector('.button--form');
 formOpener.addEventListener('click', function(e) {
   e.preventDefault();
+  closeAllModals();
   var formModal = document.querySelector('.modal--contact');
   formModal.classList.add('shown');
 });

fd027e02c11b304c6d392ac227ae61fd2509a122
muhammadjewel <msuratov@gmail.com>
2019-06-28 15:39:47 +0500

    Change featured slider to work with CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,3 @@
-var createSlider = function(togglerClass, togglerParentClass, slidesListClass) {
-  var togglers = document.querySelectorAll(togglerClass);
-  var togglerParent = document.querySelectorAll(togglerParentClass);
-  var slidesList = document.querySelectorAll(slidesListClass);
-
-  togglers.forEach(function(toggler) {
-    toggler.addEventListener('click', function(event) {
-      event.preventDefault();
-
-      togglerParent.forEach(function(item) {
-        item.classList.remove('active');
-      });
-
-      this.parentElement.classList.add('active');
-
-      slidesList.forEach(function(slide) {
-        slide.classList.remove('active');
-      });
-
-      var activeSlideOrder = parseInt(this.dataset.slideTo);
-      slidesList[activeSlideOrder].classList.add('active');
-    })
-  });
-};
-
-createSlider('.slider__indicator button', '.slider__indicator', '.slides-item');
-createSlider('.services-list__item button', '.services-list__item', '.services__item');
-
 // Закрытие модальных окон
 var modalClosingButtons = document.querySelectorAll('.button--close');
 modalClosingButtons.forEach(function(button) {
